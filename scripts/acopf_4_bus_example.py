4277bf93bdc8e231cee3d7268ab3011789e42535
Santiago Peñate Vera <santiago.penate.vera@gmail.com>
2025-09-05 18:28:18 +0100

    Added a couple of opf cases

--- scripts/acopf_4_bus_example.py
+++ scripts/acopf_4_bus_example.py
@@ -1,10 +1,3 @@
-# Nonlinear Optimal Power Flow (AC-OPF) tutorial
-
-
-## 4 bus grid from scratch
-
-
-```python
 import VeraGridEngine as vg
 
 # Grid instantiation
@@ -45,27 +38,3 @@ print("Buses:\n", opf_res.get_bus_df())
 print("Generators:\n", opf_res.get_gen_df())
 print("Branches:\n", opf_res.get_branch_df())
 print("error: ", opf_res.error)
-```
-
-
-## IEEE 14 OPF case
-
-```python
-import os
-import VeraGridEngine as gce
-
-fname = os.path.join('..', 'data', 'pglib_opf', 'pglib_opf_case14_ieee.m')
-main_circuit = gce.open_file(fname)
-
-# declare the snapshot opf
-opf_options = gce.OptimalPowerFlowOptions(solver=gce.SolverType.NONLINEAR_OPF,
-                                          ips_tolerance=1e-6,
-                                          ips_iterations=40)
-opf_driver = gce.OptimalPowerFlowDriver(grid=main_circuit, options=opf_options)
-opf_driver.run()
-
-opf_res: gce.OptimalPowerFlowResults = opf_driver.results
-print("Buses:\n", opf_res.get_bus_df())
-print("Generators:\n", opf_res.get_gen_df())
-print("Branches:\n", opf_res.get_branch_df())
-```
